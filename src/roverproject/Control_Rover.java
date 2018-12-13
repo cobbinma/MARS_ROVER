63d618556329155ef8e0bde92b05c0e9cb5a67fa
cobbinma <cobbinma@gmail.com>
2018-12-13 17:26:23 +0000

    add comments

--- src/roverproject/Control_Rover.java
+++ src/roverproject/Control_Rover.java
@@ -1,3 +1,10 @@
+/**@author Matthew Cobbing
+ * Control_Rover class v1.0
+ * Takes input from NASA text file and finds end location
+ * of each rover by using Rover class. Output is sent to
+ * console.
+ * TODO:Implement rover collision detection.
+ */
 package roverproject;
 
 import java.io.File;
@@ -9,31 +16,32 @@ public class Control_Rover {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		printRoverPositionsFromFile("input.txt");
-
 	}
 	
 	public static void printRoverPositionsFromFile(String fileName) {
-		int limitX;
-		int limitY;
-		int x;
-		int y;
+		int limitX; int limitY;
+		int x; int y;
 		String heading;
 		String path;
 		try {
 			File file = new File(fileName);
 			Scanner scanner = new Scanner(file);
+			//All data in text file is assumed to be correct.
 			limitX = scanner.nextInt();
 			limitY = scanner.nextInt();
+			//loops text file until each rover is moved.
 			while (scanner.hasNext()) {
 				x = scanner.nextInt();
 				y = scanner.nextInt();
 				heading = scanner.next();
 				path = scanner.next();
+				//create rover object and then print to console.
 				Rover newRover = new Rover(x, y, heading, limitX, limitY);
 				System.out.println(Rover.findPosition(newRover, path));
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
+			System.out.println("Problem reading file: ");
 			e.printStackTrace();
 		}
 	}
--- src/roverproject/Rover.java
+++ src/roverproject/Rover.java
@@ -1,3 +1,8 @@
+/**@author Matthew Cobbing
+ * Rover class v1.0
+ * Controls movement of individual NASA rover on mars plateau
+ * TODO:Implement rover collision detection.
+ */
 package roverproject;
 
 public class Rover {
@@ -6,6 +11,7 @@ public class Rover {
 	String heading;
 	int limitX;
 	int limitY;
+	boolean gridArray[][];
 
 	public Rover(int x, int y, String h, int limitX, int limitY) {
 		this.x = x;
@@ -15,18 +21,20 @@ public class Rover {
 		this.limitY = limitY;
 	}
 
+	//method moves rover to new position depending on heading and
+	//will only move if the rover will stay on plateau after moving.
 	public static void moveForward(Rover newRover) {
-		if (newRover.heading.equals("N") && (newRover.y + 1 <= newRover.limitY)) {
+		if (newRover.heading.equals("N") && (newRover.y + 1 <= newRover.limitY)){
 			newRover.y++;
-		} else if (newRover.heading.equals("E") && (newRover.x + 1 <= newRover.limitX)) {
+		} else if (newRover.heading.equals("E") && (newRover.x + 1 <= newRover.limitX)){
 			newRover.x++;
-		} else if (newRover.heading.equals("S") && (newRover.y - 1 >= 0)) {
+		} else if (newRover.heading.equals("S") && (newRover.y - 1 >= 0)){
 			newRover.y--;
-		} else if (newRover.heading.equals("W") && (newRover.x - 1 >= 0)) {
+		} else if (newRover.heading.equals("W") && (newRover.x - 1 >= 0)){
 			newRover.x--;
 		}
 	}
-	
+
 	public static void turnLeft(Rover newRover) {
 		if (newRover.heading.equals("N")) {
 			newRover.heading = "W";
@@ -38,7 +46,7 @@ public class Rover {
 			newRover.heading = "S";
 		}
 	}
-	
+
 	public static void turnRight(Rover newRover) {
 		if (newRover.heading.equals("N")) {
 			newRover.heading = "E";
@@ -50,19 +58,20 @@ public class Rover {
 			newRover.heading = "N";
 		}
 	}
-	
+
+	//method calculates end position of rover given the total path
 	public static String findPosition(Rover newRover, String path) {
 		char[] charArray = path.toCharArray();
-		for(int i = 0; i<charArray.length; i++) {
-			if(charArray[i] == 'M') {
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] == 'M') {
 				Rover.moveForward(newRover);
-			}else if(charArray[i] == 'L') {
+			} else if (charArray[i] == 'L') {
 				Rover.turnLeft(newRover);
-			}else if(charArray[i] == 'R') {
+			} else if (charArray[i] == 'R') {
 				Rover.turnRight(newRover);
 			}
 		}
-		return newRover.x + " " + newRover.y + " " + newRover.heading;		
+		return newRover.x + " " + newRover.y + " " + newRover.heading;
 	}
-	
+
 }
